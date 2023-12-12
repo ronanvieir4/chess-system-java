75c81af04f06b827a0ae2cd34d69270c4c4eaf4f
Ronan Vieira <ronanvieira14@gmail.com>
2023-12-12 18:03:45 -0300

    Possible moves of a piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,11 +31,7 @@ public class Program {
 
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
             }
-            catch (ChessException e) {
-                System.out.println(e.getMessage());
-                sc.nextLine();
-            }
-            catch (InputMismatchException e) {
+            catch (ChessException | InputMismatchException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
             }
